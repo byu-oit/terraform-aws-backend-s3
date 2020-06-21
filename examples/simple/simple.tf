9c7b4977afe04cc2ec1f294b9cb4dd7dbdb7bb48
Josh Gubler <josh.gubler@gmail.com>
2020-06-20 18:48:45 -0600

    add ci example so that we are sure to test the code on this branch (not some tag)

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "backend-s3" {
+module "backend_s3" {
   source = "github.com/byu-oit/terraform-aws-backend-s3?ref=v1.0.4"
 }
 
 output "s3" {
-  value = module.backend-s3.s3_bucket
+  value = module.backend_s3.s3_bucket
 }
